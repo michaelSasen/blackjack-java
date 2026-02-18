b07cb2d36a9e4216b06ad617859dba7a01efa5de
Michael Sasen Davidsen <md.vidsen@gmail.com>
2026-02-18 13:52:54 +0100

    Removed unused semis

--- src/main/java/org/example/cards/Rank.java
+++ src/main/java/org/example/cards/Rank.java
@@ -3,6 +3,14 @@ package org.example.cards;
 public enum Rank {
     ACE(11), TWO(2), THREE(3), FOUR(4), FIVE(5), SIX(6), SEVEN(7), EIGHT(8), NINE(9), TEN(10),  JACK(10), QUEEN(10), KING(10);
 
+    private final int value;
+
+    // Gir hver rang en verdi
     Rank(int i) {
+        this.value = i;
+    }
+
+    public int value() {
+        return value;
     }
 }
